a933796ad1f7851d8b28a3315660e9f546758de3
yuramorozov01 <yura_morozov01@mail.ru>
2021-12-11 18:02:09 +0300

    feat: add base repository to work with room model

---
+++ repositories/rooms_repository.py
@@ -0,0 +1,27 @@
+from repositories.repository import Repository
+
+
+class RoomsRepository(Repository):
+    _connection = Repository.get_connection()
+
+    @classmethod
+    def create_rooms_table(cls):
+        if cls._connection is not None:
+            query = (
+                "CREATE TABLE IF NOT EXISTS `rooms` ("
+                "   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,"
+                "   `name` VARCHAR(256) NOT NULL,"
+                "   PRIMARY KEY (`id`)"
+                ") ENGINE=InnoDB"
+            )
+            cls.make_query(query)
+
+    @classmethod
+    def add_room(cls, room):
+        if cls._connection is not None:
+            query = (
+                "INSERT INTO `rooms` (id, name) "
+                "VALUES (%s, %s)"
+            )
+            params = (room.get('id'), room.get('name'))
+            cls.make_query(query, params)
